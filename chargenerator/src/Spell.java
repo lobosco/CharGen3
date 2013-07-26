68153d204c3dde9d53c7f17c037f6810b50afe59
w.k <lobosco@PentCentan.(none)>
2013-07-26 17:50:29 +0200

    initialise repository

---
+++ chargenerator/src/Spell.java
@@ -0,0 +1,16 @@
+
+public class Spell {
+	
+    String Name;
+    String Prerequisites;
+    String Effect;
+    
+    public Spell(String Name, String Prerequisites, String Effect){
+    	this.Name = Name;
+    	this.Prerequisites = Prerequisites;
+    	this.Effect = Effect;
+    	
+    }
+ 
+
+}
